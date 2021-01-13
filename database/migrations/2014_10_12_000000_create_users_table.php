aeb57af8ce21b7dec244f3b787df89b56e7fc69d
bharvey-dev <65544339+bharvey-dev@users.noreply.github.com>
2021-01-13 07:38:55 -0500

    adding force reset timestatmp

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->date('birth_date')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('sms_verified_at')->nullable();
+            $table->timestamp('force_reset')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
