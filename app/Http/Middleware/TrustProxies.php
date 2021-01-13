a0b79c9f3d8bff21122f06f323e1f6ddf5c31ffd
Douglas Cockerham <douglascockerham@polk-county.net>
2021-01-13 05:28:58 -0500

    Configured TrustProxies.php to allow for the load balancer. It may be a good idea to extract this setting to an environment var.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '10.116.0.10',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
