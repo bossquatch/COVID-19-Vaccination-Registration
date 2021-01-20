08997986160054c917f44fb2cb08eda16986e732
Maliboa5150 <77059567+Maliboa5150@users.noreply.github.com>
2021-01-20 09:31:22 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'password',
         'sms_capable',
         'birth_date',
+        'age',
         'sms_verifed_at',
         'suffix_id',
     ];
