f5b0273a315873e5e668353ee61116233ea9a04d
Douglas Cockerham <padretres@gmail.com>
2021-02-01 18:28:38 -0500

    Changed horizon to use the 'queues' database on Redis instead of 'default'.
     Added 'queues' database to the redis section in database.php.
    
     Split each database into it's own redis database.
    
     Added .env.old to .gitignore.  I use this convention when backing up .env.

--- config/database.php
+++ config/database.php
@@ -145,9 +145,17 @@ return [
         'geocode-cache' => [ // choose an appropriate name
             'host' => env('REDIS_HOST', '192.168.10.10'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_GEOCODE_CACHE_DB', 2), // be sure this number differs from your other redis databases
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_GEOCODE_CACHE_DB', '2'), // be sure this number differs from your other redis databases
         ],
+
+		'queues' => [
+			'url' => env('REDIS_URL'),
+			'host' => env('REDIS_HOST', '127.0.0.1'),
+			'password' => env('REDIS_PASSWORD', null),
+			'port' => env('REDIS_PORT', '6379'),
+			'database' => env('REDIS_QUEUES_DB', '3'),
+		],
 
     ],
 
