e00bdfaa08db6cacf35a01e2a000447b311370eb
bharvey-dev <65544339+bharvey-dev@users.noreply.github.com>
2021-02-02 14:08:44 -0500

    horizon config fix 2

--- config/horizon.php
+++ config/horizon.php
@@ -193,6 +193,14 @@ return [
             ],
         ],
 
+        'test' => [
+            'supervisor-1' => [
+                'maxProcesses' => 10,
+                'balanceMaxShift' => 1,
+                'balanceCooldown' => 3,
+            ],
+        ],
+
         'local' => [
             'supervisor-1' => [
                 'maxProcesses' => 3,
