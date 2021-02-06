e42acb47efce9488d72793dad5ba3479663a2491
Douglas Cockerham <padretres@gmail.com>
2021-02-06 16:55:39 -0500

    configured the url of the button

--- resources/views/mail/invitation.blade.php
+++ resources/views/mail/invitation.blade.php
@@ -3,7 +3,7 @@
 
 Please login and accept your invitation to the COVID-19 vaccination event.
 
-@component('mail::button', ['url' => 'https://dev.register.polk.health/home','color' => 'success'])
+@component('mail::button', ['url' => config('app.url') . '/home','color' => 'success'])
     Login to your account
 @endcomponent
 
