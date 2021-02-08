9de1b42c8e7ff11bb86babfabc6b5e1184401511
Douglas Cockerham <douglascockerham@polk-county.net>
2021-02-08 17:09:29 -0500

    changed notifications for demo

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -3,9 +3,9 @@
 
 You have been confirmed for your COVID-19 vaccination appointment.  Your appointment is at **Legoland in Winter Haven, FL** (1 Legoland Way, Winter Haven, FL 33884).  Your appointment time is **8:30 AM**.
 
-<div class="text-center">
+{{--<div class="text-center">--}}
 ![QR Code]({{ env('CDN_URL') .'/images/qr-code-doug.png' }})
-</div>
+{{--</div>--}}
 
 @component('mail::button', ['url' => 'https://dev.register.polk.health/home','color' => 'doug'])
     Login to your account
