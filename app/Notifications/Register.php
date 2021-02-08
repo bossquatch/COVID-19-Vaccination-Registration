6963cc75c0d03607fd45f3bad59654d904a828bf
Douglas Cockerham <douglascockerham@polk-county.net>
2021-02-08 10:15:38 -0500

    added registration notification

---
+++ app/Notifications/Register.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Mail\Registration;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Mail;
+use NotificationChannels\Twilio\TwilioChannel;
+use Twilio\Rest\Client;
+
+class Register extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct()
+    {
+
+    }
+
+    public function viaQueues()
+    {
+        return [
+            'mail'                  => 'emails',
+            'database'              => 'database',
+            TwilioChannel::class    => 'sms',
+        ];
+    }
+
+    public function via($notifiable)
+    {
+        if ($notifiable->auto_contactable) {
+            if ($notifiable->can_sms) {
+                return ['mail','database',TwilioChannel::class];
+            } else {
+                return ['mail','database'];
+            }
+        } else {
+            return ['database'];
+        }
+    }
+
+    public function toMail($notifiable)
+    {
+
+        $emailAddress = $notifiable->user->email;
+        return Mail::to($emailAddress)
+            ->send(new Registration('Polk Health - Vaccination Registration'));
+
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'user_id' => $notifiable->id,
+        ];
+    }
+
+    public function toTwilio($notifiable)
+    {
+
+        $sid    = env('TWILIO_ACCOUNT_SID');
+        $token  = env('TWILIO_AUTH_TOKEN');
+        $twilio = new Client($sid, $token);
+
+        $message = $twilio->messages
+            ->create($notifiable->phone_number,
+                array(
+                    "messagingServiceSid" => env('TWILIO_SMS_SERVICE_SID'),
+                    "body" => $notifiable->first_name . ', you have been registered for your COVID-19 vaccination.'
+                )
+            );
+        return $message->sid;
+    }
+}
--- resources/views/mail/invitation.blade.php
+++ resources/views/mail/invitation.blade.php
@@ -1,10 +1,16 @@
 @component('mail::message')
-# You have an invitation available to an upcoming event
+# {{ $registration->suffix_id ? $registration->first_name.' '.$registration->last_name.', '.$registration->suffix->display_name : $registration->first_name.' '.$registration->last_name }}, you have an appointment!
 
-Please login and accept your invitation to the COVID-19 vaccination event.
+You have been scheduled for an appointment for your COVID-19 vaccination.  Your appointment is at {{ $location->address . ' ' . $location->city . ', ' . $location->state . ' ' . $location->zip }}.  Your appointment is between {{ \Carbon\Carbon::parse($slot->starting_at)->format('h:iA') . '-' . \Carbon\Carbon::parse($slot->ending_at)->format('h:iA') }}.  Please arrive on-time.
 
-@component('mail::button', ['url' => config('app.url') . '/home','color' => 'success'])
-    Login to your account
+Please log into the COVID-19 vaccination registration website to accept your appointment.  This offer will expire at {{ $expires }}.
+
+@component('mail::button', ['url' => 'https://register.polk.health/home'])
+    Log into the Website
 @endcomponent
 
+Remember, proof of Florida residency is ***required*** at your appointment.
+
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
