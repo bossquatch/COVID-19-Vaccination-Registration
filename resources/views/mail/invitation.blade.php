0cf4158c3041b537b9d30e7a4556340d5565d544
Douglas Cockerham <douglascockerham@polk-county.net>
2021-02-08 13:41:19 -0500

    changed notifications for demo

--- resources/views/mail/invitation.blade.php
+++ resources/views/mail/invitation.blade.php
@@ -5,7 +5,7 @@ You have been scheduled for an appointment for your COVID-19 vaccination.  Your 
 
 Please log into the COVID-19 vaccination registration website to accept your appointment.  This offer will expire **February 10, 2021 at 12:00PM**.
 
-@component('mail::button', ['url' => 'https://register.polk.health/home'])
+@component('mail::button', ['url' => 'https://dev.register.polk.health/home'])
     Log into the Website
 @endcomponent
 
