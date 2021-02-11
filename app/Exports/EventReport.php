04cba2491f379f331f463c7605344c2fa25a5cdb
bharvey-dev <65544339+bharvey-dev@users.noreply.github.com>
2021-02-11 15:20:33 -0500

    Changing spreadsheet info and formatting

--- app/Exports/EventReport.php
+++ app/Exports/EventReport.php
@@ -8,9 +8,15 @@ use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\Exportable;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 
 
-class EventReport implements FromView, Responsable
+class EventReport implements FromView, Responsable, WithColumnFormatting
 {
     use Exportable;
 
@@ -18,12 +24,12 @@ class EventReport implements FromView, Responsable
     * It's required to define the fileName within
     * the export class when making use of Responsable.
     */
-    private $fileName = 'COVID19VaccinationReport.csv';
+    private $fileName = 'COVID19VaccinationReport.xlsx';
     
     /**
     * Optional Writer Type
     */
-    private $writerType = Excel::CSV;
+    private $writerType = Excel::XLSX;
     
     /**
     * Optional headers
@@ -62,6 +68,7 @@ class EventReport implements FromView, Responsable
                 });
             })->get();
 
+            /*
         $registrations_no_vaccine = Registration::with('address')
             ->whereDoesntHave('vaccines', function ($query) {
                 $query->where('date_given', '=', $this->event_date);
@@ -70,11 +77,32 @@ class EventReport implements FromView, Responsable
                     $query->where('event_id', '=', $this->event_id);
                 })->where('invite_status_id', 10);
             })->get();
+            */
 
         return view('exports.events.report', [
             'registrations_vaccine' => $registrations_vaccine,
-            'registrations_no_vaccine' => $registrations_no_vaccine,
+            //'registrations_no_vaccine' => $registrations_no_vaccine,
             'date' => $this->event_date,
         ]);
     }
+
+    /*
+    public function bindValue(Cell $cell, $value)
+    {
+        $cell->setValueExplicit($value, DataType::TYPE_INLINE);
+
+        return true;
+    }
+    */
+
+    public function columnFormats(): array
+    {
+        return [
+            'M' => 'mm/dd/yyyy',
+            'Q' => '00000000000',
+            'R' => 'mm/yyyy',
+            'S' => 'mm/dd/yyyy',
+        ];
+    }
+
 }
