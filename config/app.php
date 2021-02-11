b5fb4419718b003f7d631d74d4b027f9e27cd1f5
Christopher Carpenter <christophercarpenter@polk-county.net>
2021-02-11 18:26:10 -0500

    Added SimpleSoftwareIO QrCode to app config

--- config/app.php
+++ config/app.php
@@ -187,7 +187,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Geocoder\Laravel\Providers\GeocoderService::class,
-
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -241,6 +241,7 @@ return [
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         // 'Redis' => Illuminate\Support\Facades\Redis::class,
