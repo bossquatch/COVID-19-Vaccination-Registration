2c2fa1d589defb372cf1a3414278e6114de01997
Christopher Carpenter <christophercarpenter@polk-county.net>
2021-02-11 17:02:38 -0500

    Added logo to QR code

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -12,7 +12,7 @@ Here are your appointment details:
 
 **Show this QR code at check-in:**
 
-<img src="{{ QrCode::format('png')->merge('https://cdn.polk.design/images/polk-logo-email.png')->generate('test') }}">
+<img src="{{ QrCode::format('png')->merge('https://cdn.polk.design/images/polk-logo-email.png', 0.3, true)->generate('test') }}">
 
 Remember, proof of Florida residency is ***required*** at your appointment.
 
