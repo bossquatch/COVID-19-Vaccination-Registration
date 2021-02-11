ef0a21e2f5caa2a8bf0e1d343d58fc1bb686e60b
Christopher Carpenter <christophercarpenter@polk-county.net>
2021-02-11 15:00:49 -0500

    Updated footer logo styles

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -3,7 +3,7 @@
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
 @if(config('mail.logo'))
-<img src="{{ config('mail.logo') }}" class="logo" alt="{{ config('mail.logo_alt') }}">
+<img src="{{ config('mail.logo') }}" alt="{{ config('mail.logo_alt') }}">
 @else
 {{ config('app.name') }}
 @endif
@@ -26,8 +26,8 @@
 @slot('footer')
 @component('mail::footer')
 <a href="{{ config('app.organization_url') }}" title="{{ config('app.organization') }}" class="logo">
-<img src="{{ config('mail.organization_logo') }}" class="logo" alt="{{ config('mail.organization_logo_alt') }}">
-</a>
+<img src="{{ config('mail.organization_logo') }}" alt="{{ config('mail.organization_logo_alt') }}">
+</a><br>
 © {{ date('Y') }} {{ config('app.organization') }}. @lang('All rights reserved.')
 @endcomponent
 @endslot
