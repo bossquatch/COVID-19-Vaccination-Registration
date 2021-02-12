a39da0423b820ac6d72010ae048cbb47d029ef27
Christopher Carpenter <christophercarpenter@polk-county.net>
2021-02-11 21:23:18 -0500

    Updated mail styles

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -10,11 +10,11 @@ Here are your appointment details:
 {{ $locationCity.', '.$locationState.' '.$locationZip }}
 @endcomponent
 
-<p class="text-center">Your registration code is:</p>
 <div class="text-center">
+<p class="text-center">Your registration code is:</p>
 {!! \SimpleSoftwareIO\QrCode\Facades\QrCode::size(150)->generate(config('app.url').'/'.$userId.'/'.$regId.'/'.$code.'?checkin=auto'); !!}
-</div>
 <p class="h2 text-center">{{ $code }}</p>
+</div>
 
 For quick check-in, log into the COVID-19 vaccination registration website and show your QR Code.
 
