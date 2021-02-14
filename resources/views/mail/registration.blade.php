3ecf488d29dfec77aae83aac6e814341fa269703
Douglas Cockerham <douglascockerham@polk-county.net>
2021-02-14 14:29:21 -0500

    Changed wording on registration email

--- resources/views/mail/registration.blade.php
+++ resources/views/mail/registration.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-# {{ $registration->suffix_id ? $registration->first_name.' '.$registration->last_name.', '.$registration->suffix->display_name : $registration->first_name.' '.$registration->last_name }}, your Polk COVID-19 Vaccine Registration is Complete
+# {{ $registration->suffix_id ? $registration->first_name.' '.$registration->last_name.', '.$registration->suffix->display_name : $registration->first_name.' '.$registration->last_name }}, your Polk COVID-19 Vaccine registration is complete
 
-Now all you need to do is wait for us to schedule you an appointment.  We will contact you with your appointment time and date.
+We will contact you soon with your appointment details.
 
 Your registration code is:
 **{{ $registration->code }}**
