b7c1b889b0d6d2ad5da7f655b6a26202175f50d3
Douglas Cockerham <douglascockerham@polk-county.net>
2021-02-14 08:08:58 -0500

    added .editorconfig to gitignore so that IDE config changes don't get pushed out.

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -3,6 +3,8 @@
 
 This is a test, it is only a test. If this were a real emergency....
 
+<img src="{!!$message->embedData(QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png')!!}">
+
 @component('mail::button', ['url' => config('app.url') . '/home','color' => 'doug'])
 Login to your account
 @endcomponent
