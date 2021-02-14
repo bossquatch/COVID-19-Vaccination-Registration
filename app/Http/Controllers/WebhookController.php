fd59b5c3f1cb08212064f64b0a122631f0fda8f0
Douglas Cockerham <douglascockerham@polk-county.net>
2021-02-14 16:34:15 -0500

    Remove the logger.  Done testing.

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,7 +12,6 @@ class WebhookController extends Controller
 {
     public function emailDelivered(Request $request)
     {
-    	Log::debug($request);
         try {
             $this->handleDelivered($request->all());
             return response('Success', 200);
