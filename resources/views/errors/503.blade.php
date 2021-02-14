ecfbb17f591db7ae0b5997f524a6eda3acb0297d
Douglas Cockerham <douglascockerham@polk-county.net>
2021-02-14 08:55:17 -0500

    Changing the maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -6,29 +6,30 @@
 
 @section('content')
 <!-- Header -->
-<div class="jumbotron jumbotron-fluid jumbotron-header bg-squares teal-gradient">
-    <div class="container position-relative z-1">
-        <div class="row">
-            <div class="col-12">
-                <!-- Badge -->
-                <span class="badge badge-pill badge-white-teal mb-3">
+
+
+<section class="main pt-8 pt-md-11 pb-8 pb-md-12">
+    <div class="container">
+		<div class="jumbotron jumbotron-fluid jumbotron-header bg-squares teal-gradient">
+			<div class="container position-relative z-1">
+				<div class="row">
+					<div class="col-12">
+						<!-- Badge -->
+						<span class="badge badge-pill badge-white-teal mb-3">
                     <span class="h6 text-uppercase">
                         503
                     </span>
                 </span>
 
-                <!-- Heading -->
-                <h2 class="title">Our site is currently down for maintenance</h2>
+						<!-- Heading -->
+						<h2 class="title">Our site is currently down for maintenance</h2>
 
-                <!-- Text -->
-                <p class="font-size-lg text-gray-dark mb-0">We will be back soon</p>
-            </div>
-        </div>
-    </div>
-</div>
-
-<section class="main pt-8 pt-md-11 pb-8 pb-md-12">
-    <div class="container">
+						<!-- Text -->
+						<p class="font-size-lg text-gray-dark mb-0">We will be back soon</p>
+					</div>
+				</div>
+			</div>
+		</div>
     </div>
 </section>
 @endsection
