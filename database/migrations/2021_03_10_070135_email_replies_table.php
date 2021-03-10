0a17b277679db391332a247413117c2f99872b1b
Douglas Cockerham <douglascockerham@polk-county.net>
2021-03-10 15:42:10 -0500

    Email reply functionality

--- database/migrations/2021_03_10_070135_email_replies_table.php
+++ database/migrations/2021_03_10_070135_email_replies_table.php
@@ -14,7 +14,7 @@ class EmailRepliesTable extends Migration
     public function up()
     {
 	    Schema::create('email_replies', function (Blueprint $table) {
-		    $table->uuid('id')->primary();
+		    $table->id('id')->primary();
 		    $table->unsignedBigInteger ('user_id')->nullable();
 		    $table->string('content_type')->nullable ();
 		    $table->dateTime('date')->nullable ();
